cfc6d5fbe89c245fe1cf506aab78e07eb83d2048
Vbeo147 <vbeo147@gmail.com>
2022-09-12 09:59:16 +0900

    Update Profile, Router, Navigation

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 export default function AppRouter({ isLoggedIn }) {
   return (
     <BrowserRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route exact path="/" element={<Home />} />
+          <>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route exact path="/" element={<Auth />} />
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,15 @@
+import { authService } from "myBase";
+import { useNavigate } from "react-router-dom";
+
 export default function Profile() {
-  return <span>Profile</span>;
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 }

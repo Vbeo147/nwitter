e9be23174ebdd833f55ebc4e2412a796d25a2069
Vbeo147 <vbeo147@gmail.com>
2022-09-12 09:27:00 +0900

    Update App, myBase, Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import AppRouter from "components/Router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { authService } from "myBase";
 
 function App() {
-  const [isLoggedIn, setisLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setisLoggedIn(true);
+      } else {
+        setisLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <div>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+    </div>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,11 @@
-import { authService } from "myBase";
+import { authService, firebaseInstance } from "myBase";
 import { useState } from "react";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -31,8 +32,22 @@ export default function Auth() {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (e) => {
+    const {
+      target: { name },
+    } = e;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   };
   return (
     <div>
@@ -53,11 +68,22 @@ export default function Auth() {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
